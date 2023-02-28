e1ee72ed1e69e4cb4d67db1a0f95742c43ba89ff
Janhavi Soni <jsoni@BLR.ALLEGISINDIA.COM>
2023-02-28 10:49:10 +0530

    Code

---
+++ JavaPractice/src/PractriceLab1/classesAndObjects.java
@@ -0,0 +1,14 @@
+package PractriceLab1;
+
+public class classesAndObjects {
+	public void print() {
+		System.out.println("Happy Coding");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		classesAndObjects co=new classesAndObjects();
+		co.print();
+	}
+
+}
